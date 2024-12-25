2130abb13e0f2e97435a8f5c2e8a8e72a50e8655
Nguyễn Phúc Khang <98502471+Khang2561@users.noreply.github.com>
2024-12-25 09:50:30 +0700

    create author

--- bookstore_backend/bookstore_backend/src/test/kotlin/com/devtiro/bookstore_backend/controllers/AuthorsControllerTest.kt
+++ bookstore_backend/bookstore_backend/src/test/kotlin/com/devtiro/bookstore_backend/controllers/AuthorsControllerTest.kt
@@ -1,9 +1,15 @@
 package com.devtiro.bookstore_backend.controllers
 
 import com.devtiro.bookstore_backend.domain.dto.AuthorDto
+import com.devtiro.bookstore_backend.domain.entity.AuthorEntity
+import com.devtiro.bookstore_backend.services.AuthorService
 import com.fasterxml.jackson.databind.ObjectMapper
-import org.assertj.core.api.Assertions.*
+import com.ninjasquad.springmockk.MockkBean
+import io.mockk.every
+import io.mockk.verify
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
+
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
@@ -14,7 +20,10 @@ import org.springframework.test.web.servlet.post
 
 @SpringBootTest
 @AutoConfigureMockMvc
-class AuthorsControllerTest @Autowired constructor(private val mockMvc: MockMvc) {
+class AuthorsControllerTest @Autowired constructor(
+    private val mockMvc: MockMvc,
+    @MockkBean val authorService: AuthorService
+) {
 
     val objectMapper = ObjectMapper()
 
@@ -38,5 +47,43 @@ class AuthorsControllerTest @Autowired constructor(private val mockMvc: MockMvc)
         }
     }
 
+    @BeforeEach
+    fun beforeEach(){
+        every {
+            authorService.save(any())
+        }answers {
+            firstArg()
+        }
+    }
+
+    @Test
+    fun `test that create Author saves the Author`(){
+        every {
+            authorService.save(any())
+        }answers {
+            firstArg()
+        }
+        mockMvc.post("/v1/authors"){
+            contentType = MediaType.APPLICATION_JSON
+            accept = MediaType.APPLICATION_JSON
+            content = objectMapper.writeValueAsString(
+                AuthorDto(
+                    id = null,
+                    name = "John Doe",
+                    age = 30,
+                    image = "author-image.png",
+                    description = "some-descriptionq.jpeg"
+                )
+            )
+        }
+        val expected = AuthorEntity(
+            id = null,
+            name = "John Doe",
+            age = 30,
+            image = "author-image.png",
+            description = "some-descriptionq.jpeg"
+        )
+        verify{authorService.save((expected))}
+    }
 
 }
